b67f1dcf7e94c4d007d9153fac017b2053b7a693
Pathernon2021 <santine.silva@bol.com.br>
2023-09-19 21:19:31 -0300

    atualizar

--- java-terminal/src/Aboutme.java
+++ java-terminal/src/Aboutme.java
@@ -1,3 +1,4 @@
+ public class Aboutme {
  public static void main(String[] args) {
         String nome =args [0];
         String sobrenome = args [1];
@@ -6,4 +7,7 @@
 
         System.out.println("Olá me chamo"+ nome +" " +sobrenome);
         System.out.println("Tenho"+ idade+" " + "anos");
-         System.out.println("Minha Altura é "+ altura +" cm" );
+        System.out.println("Minha altura é "+ altura +" cm" );
+ 
+       }
+}
